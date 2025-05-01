76c7ed928de6614c8d715999642128baa9450519
est_ufg <est_ufg@yahoo.com>
2025-05-01 16:42:52 -0600

    first commit

--- products/src/main/java/com/example/product/controller/ProductController.java
+++ products/src/main/java/com/example/product/controller/ProductController.java
@@ -1,14 +1,12 @@
 package com.example.product.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.product.dto.ProductDto;
 import com.example.product.service.ProductService;
 
 @RestController
@@ -19,13 +17,13 @@ public class ProductController {
 	private ProductService productService;
 	
 	@GetMapping
-    public List<ProductDto> getAll() {
-        return productService.getAll();
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.ok(productService.getAll());
     }
 
     @GetMapping("/{id}")
-    public ProductDto getById(@PathVariable Long id) {
-        return productService.getById(id);
+    public ResponseEntity<?> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getById(id));
     }
 
 }
